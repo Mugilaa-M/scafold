2bab494ade9a1bfc93a969288593589a492473ba
Mugilaa-tct <125123535+Mugilaa-tct@users.noreply.github.com>
2023-08-27 20:34:41 +0530

    Add project files.

---
+++ DemoScafffold/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoScafffold.MvcApplication" Language="C#" %>
---
+++ DemoScafffold/Models/EmployeeData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DemoScafffold.Models
+{
+    public class EmployeeData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public int Age { get; set; }
+    }
+
+    // To communicate with DB for retriving and saving the data Need to complete the code as follow
+
+    public class EmpDBContext : DbContext
+    {
+        public DbSet<EmployeeData> Employees { get; set; }
+    }
+
+
+}
